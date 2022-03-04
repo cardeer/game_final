f91deda05da600719ff141233a8d918c3736f0d1
コーヒー <cardeer123_1@hotmail.com>
2022-03-04 18:35:43 +0700

    add comment

--- game_final/Utils/Converter.cs
+++ game_final/Utils/Converter.cs
@@ -6,6 +6,11 @@ namespace game_final.Utils
 {
     class Converter
     {
+        /// <summary>
+        /// Change radians to degrees
+        /// </summary>
+        /// <param name="radians"></param>
+        /// <returns></returns>
         public static double RadiansToDegrees(double radians) {
             return radians * 180 / Math.PI;
         }
