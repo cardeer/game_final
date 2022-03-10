03222b392756a7221d8d0256b98f57fe9846a759
コーヒー <cardeer123_1@hotmail.com>
2022-03-10 11:36:23 +0700

    text class

---
+++ game_final/Sprites/Text.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace game_final.Sprites
+{
+    class Text : Base.Object
+    {
+        public Color Color = Color.Black;
+
+        private SpriteFont _font;
+        private string _text;
+
+        private Vector2 _measure;
+
+        public Text(SpriteFont font, string text)
+        {
+            _font = font;
+            _text = text;
+
+            _measure = _font.MeasureString(text);
+        }
+    }
+}
