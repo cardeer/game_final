31427179661e95f46a2490f48956f500806e492c
LIN, YU-TE <eiwterlin@hotmail.com>
2022-03-10 12:29:41 +0700

    hourglass & timeBoard

---
+++ game_final/Sprites/IconButton.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace game_final.Sprites
+{
+	class IconButton : Base.Object
+    {
+        public bool Effect = true;
+        public Color TextColor = Color.White;
+        public bool MultipleClicks = false;
+
+        private Texture2D _texture;
+
+        private float _effectScale = 1.2f;
+        private float _maxEffectScale = 1.5f;
+        private bool _atMinScale = false;
+        private int _effectDirection = 1;
+
+        private SpriteFont _font;
+        private string _text;
+        private Vector2 _measureString;
+
+        private bool _justEntered = false;
+
+        public IconButton(Texture2D texture, SpriteFont font, int width, int height)
+        {
+            _texture = texture;
+
+            _font = font;
+
+            _width = width;
+            _height = height;
+
+            SetOrigin(_width / 2, _height / 2);
+        }
+
+        public void SetTexture(Texture2D texture)
+        {
+            _texture = texture;
+        }
+
+        public void Update()
+        {
+            if (IsHovering)
+            {
+                if (!_justEntered)
+                {
+                    Environments.Global.HoveringButton = true;
+                    _justEntered = true;
+                }
+
+                if (Effect)
+                {
+                    if (!_atMinScale)
+                    {
+                        _effectScale -= 3f * (float)Environments.Global.GameTime.ElapsedGameTime.TotalSeconds;
+
+                        if (_effectScale < 1)
+                        {
+                            _atMinScale = true;
+                        }
+                    }
+                    else
+                    {
+                        _effectScale += 1f * (float)Environments.Global.GameTime.ElapsedGameTime.TotalSeconds * _effectDirection;
+
+                        if (_effectScale > 1.2 || _effectScale < 1)
+                        {
+                            _effectDirection *= -1;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                _atMinScale = false;
+                _effectDirection = 1;
+                _effectScale = _maxEffectScale;
+
+                if (_justEntered)
+                {
+                    Environments.Global.HoveringButton = false;
+                    _justEntered = false;
+                }
+            }
+
+            if (IsClicked)
+            {
+                AssetTypes.Sound.ButtonClick.Play(0.2f, 0, 0);
+
+                if (!MultipleClicks)
+                {
+                    Environments.Global.HoveringButton = false;
+                }
+
+                InvokeClick(this);
+            }
+        }
+
+        public void Draw()
+        {
+            Rectangle hitBox = HitBox;
+
+            Environments.Global.SpriteBatch.Draw(_texture, hitBox, Color.White);
+
+            if (IsHovering && Effect)
+            {
+                int newWidth = (int)(_width * _effectScale);
+                int newHeight = (int)(_height * _effectScale);
+
+                Environments.Global.SpriteBatch.Draw(_texture, new Rectangle((int)(X - Origin.X + Width / 2 - newWidth / 2), (int)(Y - Origin.Y + Height / 2 - newHeight / 2), newWidth, newHeight), Color.Gray * 0.5f);
+            }
+
+            if (!string.IsNullOrEmpty(_text))
+            {
+                var x = (hitBox.X + (hitBox.Width / 2)) - (_measureString.X / 2);
+                var y = (hitBox.Y + (hitBox.Height / 2)) - (_measureString.Y / 2);
+
+                Environments.Global.SpriteBatch.DrawString(_font, _text, new Vector2(x, y), TextColor);
+            }
+        }
+    }
+}
